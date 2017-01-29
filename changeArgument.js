c453b97f898c709beb1161faac15787fa9e9cb82
fabricatedmind <fabricatedmind@gmail.com>
2017-01-29 15:37:40 -0500

    asdf

--- changeArgument.js
+++ changeArgument.js
@@ -4,7 +4,7 @@ function infiltrate(person){
     console.log("PERSON: "+person);
     console.log("ARGUMENT: "+arguments[0]);
     arguments[0] = "blah";
-
+    
     console.log("PERSON AGAIN: "+person);
     console.log("ARGUMENT AGAIN: "+arguments[0]);
 }
